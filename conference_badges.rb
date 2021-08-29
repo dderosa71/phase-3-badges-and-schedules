92af31ad2c6f738073a330e6873abe982155a947
Dominic DeRosa <dderosa71@gmail.com>
2021-08-29 11:58:33 -0500

    commit tree 7554cf8e191c4ee678fada8e234b6e72777ed332

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,24 @@
 # Write your code here.
+def badge_maker(name)
+    "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(name)
+    name.map do |item| badge_maker(item)
+    end
+end
+
+def assign_rooms(names)
+    list = []
+    names.each_with_index {|name, index|
+    list << "Hello, #{name}! You'll be assigned to room #{index+1}!"
+}
+list 
+end 
+
+def printer(array)
+    badges = batch_badge_creator(array)
+    rooms = assign_rooms(array)
+    badges.each {|item| puts item}
+    rooms.each {|item| puts item}
+end
